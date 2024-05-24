5ad52195088c369030f002161e4d9273f81f193a
stellarsable7 <chloepeh05@gmail.com>
2024-05-24 15:28:38 +0800

    Added second screen to events

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function SettingsScreen() {
+function EventsScreen({ navigation }) {
+
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
+            <Text>Events go here!</Text>
+            <Button onPress={() => { navigation.navigate("Event Details") }} title="See Details" />
         </View>
+    );
+}
+
+function EventsSecondScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Events second screen!</Text>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function EventsStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Events' component={EventsScreen} />
+            <Stack.Screen name="Event Details" component={EventsSecondScreen} />
+        </Stack.Navigator>
     );
 }
